b8d890c2ff228e3cbdda58b225fd5e1050f7434b
Burçin Akan <burcinakanweb@gmail.com>
2025-02-25 01:18:27 +0300

    navbar add picture

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
+import { HttpClient } from '@angular/common/http';
 import { Menubar } from 'primeng/menubar';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../_service/auth.service';
 import { Image } from 'primeng/image';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -14,10 +16,40 @@ import { Image } from 'primeng/image';
 })
 export class NavbarComponent {
   isLoggedIn$;
-  constructor(private router: Router,private authService: AuthService) {
+  res: any = {};  // res objesi tanımlandı
+  userId: string | null = null;
+  profileImage: string = '';
+
+  constructor(private http: HttpClient,private router: Router,private authService: AuthService) {
     this.isLoggedIn$ = this.authService.isLoggedIn$;
   }
+  ngOnInit(): void {
+    this.userId = this.authService.decodeToken().userId;
+    this.getUsers();
+  }
+
+  async getUsers() {
+    try {
+      this.res = await firstValueFrom(
+        this.http.get(`https://localhost:7273/api/User/GetUserById?userId=${this.userId}`)
+      );
+
+      if (this.res) {
+        
+        this.profileImage = `https://localhost:7273/${this.res.profileImage}`;
+    
+      }
+
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
+  }
+
   logout() {
     this.authService.logout()
   }
+
+
+
+
 }
